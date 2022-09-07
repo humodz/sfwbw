aefb058298f4c550e924c3c27e0b795243630d04
Hugo Dzin <humodz065@gmail.com>
2022-09-07 10:57:56 -0300

    add: simpledotcss, favicon; remove: default icons, manifest, robots

--- sfwbw-frontend/src/App.tsx
+++ sfwbw-frontend/src/App.tsx
@@ -3,22 +3,44 @@ import { increment, incrementByAmount, selectCount } from './store/counterSlice'
 import { useAppDispatch, useAppSelector } from './store/hooks';
 
 export function App() {
-  const dispatch = useAppDispatch();
-  const count = useAppSelector(selectCount);
-
-  useEffect(() => {
-    const id = setInterval(() => {
-      dispatch(increment());
-    }, 1000);
-
-    return () => clearInterval(id);
-  }, []);
-
-  const reset = () => {
-    dispatch(incrementByAmount(-count));
-  };
-
   return (
-    <p onClick={reset}>Hello World {count}</p>
+    <>
+      <header>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/">Editor</a>
+          <a href="https://example.com">Github</a>
+        </nav>
+
+        <h1>Super Famicom Wars by Web</h1>
+        {/* <p>subtitle</p> */}
+      </header>
+      <main>
+        <h2>Hello World</h2>
+      </main>
+      <footer>
+      </footer>
+    </>
   );
 }
+
+  export function App2() {
+    const dispatch = useAppDispatch();
+    const count = useAppSelector(selectCount);
+
+    useEffect(() => {
+      const id = setInterval(() => {
+        dispatch(increment());
+      }, 1000);
+
+      return () => clearInterval(id);
+    }, []);
+
+    const reset = () => {
+      dispatch(incrementByAmount(-count));
+    };
+
+    return (
+      <p onClick={reset}>Hello World {count}</p>
+      );
+    }
--- sfwbw-frontend/src/index.tsx
+++ sfwbw-frontend/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import 'simpledotcss';
 import { store } from './store';
 import { App } from './App';
 import './index.css';
 
-const container = document.getElementById('root')!;
+const container = document.body;
 const root = createRoot(container);
 
 root.render(
