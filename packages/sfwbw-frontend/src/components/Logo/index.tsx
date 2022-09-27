35df2713ddc353778f8d6a75819f24bbd462d8ee
Hugo Dzin <humodz065@gmail.com>
2022-09-26 21:06:40 -0300

    logo

--- packages/sfwbw-frontend/src/components/Logo/index.tsx
+++ packages/sfwbw-frontend/src/components/Logo/index.tsx
@@ -5,20 +5,9 @@ export function Logo() {
   return (
     <div className={styles.logo}>
       <img src={logo} alt="Super Famicom Wars" />
-      <strong style={{ textShadow: textBorder('#000') }}>by Web</strong>
+      <svg>
+        <text y="1em">by Web</text>
+      </svg>
     </div>
   );
-}
-
-function textBorder(color: string) {
-  return `
-    2px 0 ${color},
-    -2px 0 ${color},
-    0 2px ${color},
-    0 -2px ${color},
-    1px 1px ${color},
-    -1px -1px ${color},
-    1px -1px ${color},
-    -1px 1px ${color}
-  `;
 }
